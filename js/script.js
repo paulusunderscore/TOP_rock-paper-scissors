584665543709cf407ce6e099addc6374f4aa3e24
paulus <paulusojak@gmail.com>
2024-02-12 18:36:28 +0700

    add function to get player input and to play

--- js/script.js
+++ js/script.js
@@ -1,6 +1,45 @@
+let computerSelection = "";
+let playerSelection = "";
+let result = "";
+
 function getComputerChoice() {
     const choice = ['rock', 'paper', 'scissors'];
     const result = Math.floor(Math.random() * choice.length);
-    const computerChoice = choice[result];
-    return computerChoice;
+    return computerSelection = choice[result];
 }
+
+function getPlayerInput(input) {
+    playerInput = prompt("Choose between paper, rock, or scissors: ");
+    return playerSelection = playerInput.toLowerCase();
+}
+
+function playRound(computerSelection, playerSelection) {
+    if (computerSelection === playerSelection) {
+        result = "It's a draw!";
+    }
+    else if (computerSelection === "rock" && playerSelection === "paper") {
+        result = "Player wins!";
+    }
+    else if (computerSelection === "rock" && playerSelection === "scissors") {
+        result = "Computer wins!";
+    }
+    else if (computerSelection === "paper" && playerSelection === "rock") {
+        result = "Computer wins!";
+    }
+    else if (computerSelection === "paper" && playerSelection === "scissors") {
+        result = "Player wins!";
+    }
+    else if (computerSelection === "scissors" && playerSelection === "rock") {
+        result = "Player wins!";
+    }
+    else if (computerSelection === "scissors" && playerSelection === "paper") {
+        result = "Computer wins!";
+    }
+    return result;
+}
+
+getComputerChoice();
+getPlayerInput();
+console.log("computer: ", computerSelection);
+console.log("player: ", playerSelection);
+console.log(playRound(computerSelection, playerSelection));
